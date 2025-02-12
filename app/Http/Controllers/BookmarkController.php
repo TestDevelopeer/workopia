1c7efc57e5fb6dff7edc0fe1de3df81131ad4299
johndoe <johndoe@example.com>
2025-02-12 11:03:53 +0300

    bookmarking jobs

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
 
 class BookmarkController extends Controller
 {
@@ -14,7 +16,23 @@ class BookmarkController extends Controller
 	{
 		$user = Auth::user();
 
-		$bookmarks = $user->bookmarkedJobs()->paginate(6);
+		$bookmarks = $user->bookmarkedJobs()->orderBy('job_user_bookmarks.created_at', 'desc')->paginate(6);
 		return view('jobs.bookmarked')->with('bookmarks', $bookmarks);
+	}
+
+	// @desc Create new bookmarked job
+	// @route POST /bookmarks/{$job}
+	public function store(Job $job): RedirectResponse
+	{
+		$user = Auth::user();
+
+		// Check if the job is alreade bookmarked
+		if ($user->bookmarkedJobs()->where('job_id', $job->id)->first()) {
+			return back()->with('error', 'Job is already bookmarked');
+		}
+
+		// Create new bookmark
+		$user->bookmarkedJobs()->attach($job->id);
+		return back()->with('success', 'Job bookmarked successfully');
 	}
 }
--- routes/web.php
+++ routes/web.php
@@ -38,4 +38,5 @@ Route::middleware('auth')->group(function () {
 	Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 	Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
 	Route::get('/bookmarks', [BookmarkController::class, 'index'])->name('bookmarks.index');
+	Route::post('/bookmarks/{job}', [BookmarkController::class, 'store'])->name('bookmarks.store');
 });
