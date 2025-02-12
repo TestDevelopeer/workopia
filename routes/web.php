d71bdb016688af302691056d6690b1cdd197d592
johndoe <johndoe@example.com>
2025-02-12 15:28:00 +0300

    applicant controller & store method

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
-use App\Http\Controllers\BookmarkController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\BookmarkController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ApplicantController;
+use App\Http\Controllers\DashboardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -40,4 +41,5 @@ Route::middleware('auth')->group(function () {
 	Route::get('/bookmarks', [BookmarkController::class, 'index'])->name('bookmarks.index');
 	Route::post('/bookmarks/{job}', [BookmarkController::class, 'store'])->name('bookmarks.store');
 	Route::delete('/bookmarks/{job}', [BookmarkController::class, 'destroy'])->name('bookmarks.destroy');
+	Route::post('/jobs/{job}/apply', [ApplicantController::class, 'store'])->name('applicant.store');
 });
